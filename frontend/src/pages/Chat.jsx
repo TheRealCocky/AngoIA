274df42f7545dd580237f50cc5660c2023d91444
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-06-20 19:12:50 +0100

    Remove importação não utilizada do SettingsModalSM

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -5,7 +5,7 @@ import { Mic, AppWindow, X, Bell, Settings, House } from 'lucide-react';
 import { Link, useNavigate } from "react-router-dom";
 import { jwtDecode } from 'jwt-decode';
 import SettingsModalLg from "../components/SettingsModalLG.jsx";
-import SettingsModalSm from "../components/SettingsModalSM.jsx";
+
 
 // Função que faz requisição ao backend (enviando a mensagem do usuário e recebendo a resposta)
 const callBackendAPI = async (message) => {
